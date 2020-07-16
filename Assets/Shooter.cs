5862f3dd920cd7635183c7b7cb0d9c7d54c3b158
sho-Uber <shogo1212mail@yahoo.co.jp>
2020-07-16 23:47:55 +0900

    prefabを生成するinstantiate関数

---
+++ Assets/Shooter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+    public GameObject candyPrefab;
+    public float shotForce;
+    public float shotTorque;
+
+    // Update is called once per frame
+    void Update () {
+        if (input.GetButtonDown ("Fire1")) Shot ();
+    }
+    public void Shot () {
+        GameObject candy = INstantiate (
+            candyPrefab,
+            transform.position,
+            Quaternion.identity
+        );
+        Rigidbody candyRigidBody = candy.GetComponent<Rigidbody> ();
+        candyRigidBody.AddForce (transform.forward * shotForce);
+        candyRigidBody.AddTorque (new Vector3 (0, shotTorque, 0));
+    }
+}
