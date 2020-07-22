4fa4c3b9ab26eda3a8c6896725e5ace895b57ae8
sho-Uber <shogo1212mail@yahoo.co.jp>
2020-07-23 08:46:45 +0900

    キャンディーをランダムで生成。 生成位置をマウスの位置に設定。 落ちたキャンディーをデストロイする。

--- Assets/Shooter.cs
+++ Assets/Shooter.cs
@@ -3,22 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Shooter : MonoBehaviour {
-    public GameObject candyPrefab;
+    public GameObject[] candyPrefabs;
+    public Transform candyParentTransform;
     public float shotForce;
     public float shotTorque;
+    public float baseWidth;
 
     // Update is called once per frame
     void Update () {
-        if (Input.GetButtonDown ("Fire1")) Shot ();
+        if (Input.GetButtonDown ("Fire1")) {
+            Shot ();
+        }
     }
+
+    GameObject SampleCandy () {
+        int index = Random.Range (0, candyPrefabs.Length);
+        return candyPrefabs[index];
+    }
+
+    Vector3 GetInstantiatePosition () {
+        float x = baseWidth *
+            (Input.mousePosition.x / Screen.width) - (baseWidth / 2);
+        return transform.position + new Vector3 (x, 0, 0);
+    }
+
     public void Shot () {
-        GameObject candy = Instantiate (
-            candyPrefab,
-            transform.position,
+        GameObject candy = (GameObject) Instantiate (
+            SampleCandy (),
+            GetInstantiatePosition (),
             Quaternion.identity
         );
+
+        candy.transform.parent = candyParentTransform;
+
         Rigidbody candyRigidBody = candy.GetComponent<Rigidbody> ();
         candyRigidBody.AddForce (transform.forward * shotForce);
-        candyRigidBody.AddTorque (new Vector3 (0, shotTorque, 0));
+        candyRigidBody.AddTorque (new Vector3 (shotTorque, shotTorque, shotTorque));
     }
 }
---
+++ Assets/candyDestroyer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class candyDestroyer : MonoBehaviour {
+    void OnTriggerEnter (Collider other) {
+        if (other.gameObject.tag == "Candy") {
+            Destroy (other.gameObject);
+        }
+    }
+}
